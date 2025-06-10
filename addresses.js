e576e5351af9614fc7ad3832105322a132c90512
FreedomPros <christineeremija@solarpros.io>
2025-06-10 14:13:23 -0700

    NEWtest addresses.js

---
+++ addresses.js
@@ -0,0 +1,166 @@
+window.addresses = [
+  {
+    name: "FURY (150 - 200) CA",
+    address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
+    coords: [33.6644, -117.7576],
+    contacts: [
+      {
+        team: "FURY",
+        people: [
+          { name: "Jay Merrifield", phone: "801-336-6924" }
+        ]
+      }
+    ],
+    date: "June 26 - 27"
+  },
+  {
+    name: "MOMENTVS (70-80) CA",
+    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    coords: [34.0921, -117.5737],
+    contacts: [
+      {
+        team: "MOMENTVS",
+        people: [
+          { name: "Cole Sexton", phone: "320-339-1204" },
+          { name: "Christian Harrison", phone: "(678) 702-7185" }
+        ]
+      }
+    ],
+    date: "June 23 - 24"
+  },
+  {
+    name: "DAUNTLESS & MOTION (60-70) CA",
+    address: "15601 Garland Cir. Westminster CA 92683",
+    coords: [33.7436, -117.9677],
+    contacts: [
+      {
+        team: "DAUNTLESS",
+        people: [
+          { name: "Casey Mosier", phone: "801-787-2621" }
+        ]
+      },
+      {
+        team: "MOTION",
+        people: [
+          { name: "Jacob Garza", phone: "(623) 308-9737" },
+          { name: "Tilo Chen", phone: "(510) 402-9699" }
+        ]
+      }
+    ],
+    date: "June 30 - July 1"
+  },
+  {
+    name: "BAN, UNITED, & FORGE (65-75) CA",
+    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    coords: [34.0921, -117.5743],
+    contacts: [
+      {
+        team: "BAN",
+        people: [
+          { name: "Grant Misbach", phone: "(801) 643-9206" }
+        ]
+      },
+      {
+        team: "UNITED",
+        people: [
+          { name: "Casey Kendall", phone: "(206) 554-1489" },
+          { name: "Cameron Kendall", phone: "(206) 661-4244" }
+        ]
+      },
+      {
+        team: "FORGE",
+        people: [
+          { name: "Logan Berger", phone: "(208) 954-1233" }
+        ]
+      }
+    ],
+    date: "July 3 - 4"
+  },
+  {
+    name: "THE SPARTANS & EMPIRE (15) CA",
+    address: "27141 Hideaway Ave. Santa Clarita, 91351",
+    coords: [34.4264, -118.4842],
+    contacts: [
+      {
+        team: "THE SPARTANS",
+        people: [
+          { name: "Spencer deavila/ Zak Hattab", phone: "(484) 401-0077" }
+        ]
+      },
+      {
+        team: "EMPIRE",
+        people: [
+          { name: "Sebastian Rodriguez", phone: "(813) 679-0944" }
+        ]
+      }
+    ],
+    date: "July 7"
+  },
+  {
+    name: "PRIME & APOLLO (30) CA",
+    address: "FreedomForever: 3010 San Antonio Dr. Fowler, CA 93625",
+    coords: [36.6177, -119.6782],
+    contacts: [
+      {
+        team: "PRIME",
+        people: [
+          { name: "Hayden Young", phone: "385-219-0979" }
+        ]
+      },
+      {
+        team: "APOLLO",
+        people: [
+          { name: "Nate Girsberger", phone: "(385) 210-7846" }
+        ]
+      }
+    ],
+    date: "July 9"
+  },
+  {
+    name: "MOTION (50+) CA",
+    address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
+    coords: [37.7038, -121.7680],
+    contacts: [
+      {
+        team: "MOTION",
+        people: [
+          { name: "Junior Bandeira", phone: "773-501-5285" }
+        ]
+      }
+    ],
+    date: "July 11 - 12"
+  },
+  {
+    name: "EMPIRE, VITALITY, ENVISION & EXTREME (85-90) IL",
+    address: "4300 Commerce Ct, unit 240 Lisle, IL 60532",
+    coords: [41.8023, -88.0884],
+    contacts: [
+      {
+        team: "EMPIRE",
+        people: [
+          { name: "Rocky Niger", phone: "(727) 748-6089" }
+        ]
+      },
+      {
+        team: "VITALITY",
+        people: [
+          { name: "Chayse Kidd", phone: "(208) 360-3024" }
+        ]
+      },
+      {
+        team: "ENVISION",
+        people: [
+          { name: "Drew Sanders", phone: "(918) 706-5054" }
+        ]
+      },
+      {
+        team: "EXTREME",
+        people: [
+          { name: "Ben Pappas", phone: "(717) 925-6924" },
+          { name: "Tyler Whimpey", phone: "(801) 696-8581" }
+        ]
+      }
+    ],
+    date: "July 15 - 16"
+  }
+];
